7f6033e75d1353a7530de4d347d54b96d0a6cb79
Simon Heybrock <simon.heybrock@esss.se>
2019-07-23 11:26:08 +0200

    Extend dream script with unit conversion and basic instrument geometry.

--- dream.py
+++ dream.py
@@ -24,8 +24,28 @@ d['sample'].coords[Dim.Tof][Dim.Position, 0].values = np.arange(10)
 # d['sample'].coords[Dim.Tof].values[1] = np.arange(10)
 print(d)
 
-# Convert units (will work soon, see scipp/#267)
-# d.convert(Dim.Tof, Dim.DSpacing)
+def make_component_info(source_pos, sample_pos):
+    """
+    Create a dataset containing basic information about the components in an instrument.
+
+    Currently this contains only source and sample position.
+    The structure of this is subject to change.
+    """
+    component_info = sc.Dataset(
+            {'position': sc.Variable(
+                dims=[Dim.Row], shape=(2,),
+                dtype=sc.dtype.vector_3_double,
+                unit=sc.units.m)})
+    component_info['position'].values[0] = source_pos
+    component_info['position'].values[1] = sample_pos
+    return component_info
+
+# Add component_info, a special name that `scipp.neutron.convert` will inspect for instrument information.
+d.set_labels('component_info', sc.Variable(dtype=sc.dtype.Dataset))
+d.labels['component_info'].value = make_component_info(source_pos=[0,0,-20], sample_pos=[0,0,0])
+
+dspacing = sc.neutron.convert(d, Dim.Tof, Dim.DSpacing)
+print(dspacing)
 
 # Converting event data to histogram (not available yet)
 # hist = sc.histogram(d)
