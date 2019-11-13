7d18c73bddd368dea49b697fc65fa2374a61c122
mads-bertelsen <Mads.Bertelsen@esss.se>
2019-11-13 11:13:07 +0100

    Review response
    Consolidated the three loops into one by using min and max for index handling.

--- smooth_data.py
+++ smooth_data.py
@@ -33,20 +33,9 @@ def smooth_data(variable, dim=None, NPoints=3):
 
     hr = NPoints//2  # half range rounded down
 
-    # Split main loop into 3 parts, start, main and end.
-    for index in range(0, hr):  # Start is before reaching half range
-        start_index = 0  # Always start at 0
-        end_index = int(index + hr + 1)
-        out[dim, index] = sc.mean(variable[dim, start_index:end_index], dim)
-
-    for index in range(hr, data_length - hr):  # Main is the bulk of the data
-        start_index = int(index - hr)
-        end_index = int(index + hr + 1)
-        out[dim, index] = sc.mean(variable[dim, start_index:end_index], dim)
-
-    for index in range(data_length - hr, data_length):  # End part
-        start_index = int(index - hr)
-        end_index = data_length  # Always use data length as endpoint
-        out[dim, index] = sc.mean(variable[dim, start_index:end_index], dim)
+    for index in range(data_length):
+        begin = max(0, index - hr)
+        end = min(data_length, index + hr + 1)
+        out[dim, index] = sc.mean(variable[dim, begin:end], dim)
 
     return out
