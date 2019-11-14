99bca489227db518ee0969fae1fd66aa9c832266
Piotr Rozyczko <piotr.rozyczko@esss.se>
2019-11-14 11:43:15 +0100

    Added curve smoothing

--- spline_background.py
+++ spline_background.py
@@ -21,11 +21,14 @@ def bspline_background(variable, x_values, dim=None):
         raise ValueError("bspline_background: dimension must be specified.")
 
     values = variable[dim, :].values
+    errors = variable[dim, :].variances
+    weights = [1.0/i*i for i in errors]
 
-    # find out the knots.
-    # "Magic" s=0 for the smoothness parameter stands for interpolation,
-    # since no weights are provided.
-    knots, u = interpolate.splprep([x_values, values], s=0)
+    # spline smoothing from
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.UnivariateSpline.html
+    s = len(values)*np.std(errors)*np.std(errors)
+    # find out the knots and splines.
+    knots, u = interpolate.splprep([x_values, values], s=s, k=5, w=weights)
     # perform the B-spline interpolation
     splined_variable = interpolate.splev(u, knots)
     # splined_variable is a [2][data_length] array for X and Y values
@@ -41,6 +44,7 @@ if __name__ == '__main__':
 
     plt.rcParams['figure.figsize'] = [10, 8]
 
+    # test 1
     N = 100
     x = np.arange(N)
     y0 = np.zeros(N)
@@ -48,7 +52,15 @@ if __name__ == '__main__':
     for i, item in enumerate(y):
         xi = (15.0 / (N-1)) * i
         y[i] = np.cos(xi) * np.exp(-0.1*xi)
-    err = np.sqrt(y)
+    err = np.sqrt(y*y)
+
+    # test 2
+    # x = np.arange(30)
+    # y0 = 20.+50.*np.exp(-(x-8.)**2./120.)
+    # err = np.sqrt(y0)
+    # y = 20.+50.*np.exp(-(x-8.)**2./120.)
+    # y += err*np.random.normal(size=len(err))
+    # err = np.sqrt(y)
 
     input_y = sc.Variable(dims=[Dim.Tof], values=y, variances=err**2, unit=sc.units.us)
     output = bspline_background(input_y, x, dim=Dim.Tof)
