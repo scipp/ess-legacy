7f7be13525c3a413f705a50791826e1f4710cbf0
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-03-05 15:23:54 +0000

    Upgrade to newer scipp
    Basically no new features here other than the use of nan_to_num

--- make_config.py
+++ make_config.py
@@ -5,16 +5,16 @@ import os
 
 if __name__ == '__main__':
     # configure arg parser
-    parser = argparse.ArgumentParser(description='Makes local non-versioned config.py for working with scripts ouside of own-cloud. Generates valid config.py with the correct local paths set', prog='make_config')
+    parser = argparse.ArgumentParser(description='Makes local non-versioned scippconfig.py for working with scripts ouside of own-cloud. Generates valid scippconfig.py with the correct local paths set', prog='make_config')
     positional_args = parser.add_argument_group('Positional arguments')
     optional_args = parser.add_argument_group('Optional arguments')
     positional_args.add_argument('Root', nargs='?', help='The absolute path to the local own-cloud root script directory (processing)')
-    optional_args.add_argument('-f', '--force', action='store_true', default=False, help='Force overwrite of existing config')
+    optional_args.add_argument('-f', '--force', action='store_true', default=False, help='Force overwrite of existing scippconfig')
     # run the arg parser
     arguments = parser.parse_args()
     existing_config = False
     try:
-        import config
+        import scippconfig
         existing_config = True
     except ImportError:
         pass
@@ -23,8 +23,8 @@ if __name__ == '__main__':
     if not os.path.exists(arguments.Root):
         raise ValueError('Path {} does not exist on local machine'.format(arguments.Root))
     if existing_config and not arguments.force:
-        raise RuntimeError('config.py already exists. cannot overwrite without force option. see help.')
+        raise RuntimeError('scippconfig.py already exists. cannot overwrite without force option. see help.')
     # make the config.py
-    with open('config.py', 'w') as fh:
+    with open('scippconfig.py', 'w') as fh:
         fh.write('script_root="{}"\n'.format(arguments.Root))
-    print('config.py written')
+    print('scippconfig.py written')
