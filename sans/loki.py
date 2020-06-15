e2bb6082d0654222f4a6a75a97e9734a471abb04
Simon Heybrock <simon.heybrock@esss.se>
2020-06-15 07:27:17 +0200

    Add function for generating layer coloring

--- sans/loki.py
+++ sans/loki.py
@@ -1,8 +1,9 @@
 class LoKI:
-    def __init__(self, ntube=32, nstraw=7, npixel=512):
+    def __init__(self, ntube=32, nstraw=7, npixel=512, dim='spectrum'):
         self._ntube = ntube
         self._nstraw = nstraw
         self._npixel = npixel
+        self._dim = dim
 
     def _assert_valid_tube(self, tube):
         assert tube >= 0 and tube < self._ntube
@@ -13,11 +14,31 @@ class LoKI:
     def tube(self, tube):
         self._assert_valid_tube(tube)
         n = self._nstraw * self._npixel
-        return ('spectrum', slice(n * tube, n * (tube + 1)))
+        return (self._dim, slice(n * tube, n * (tube + 1)))
 
     def straw(self, tube, straw):
         self._assert_valid_tube(tube)
         self._assert_valid_straw(straw)
         start = (tube * self._nstraw + straw) * self._npixel
         end = (tube * self._nstraw + straw + 1) * self._npixel
-        return ('spectrum', slice(start, end))
+        return (self._dim, slice(start, end))
+
+    def layers(self):
+        import scipp as sc
+        import numpy as np
+        pixel_layers = 0
+        straw_layers = 3
+        tube_layers = 4
+        pixel_layer = sc.Variable(dims=['pixel'],
+                                  dtype=sc.dtype.int32,
+                                  values=np.zeros(self._npixel))
+        straw_layer = sc.Variable(dims=['straw'],
+                                  dtype=sc.dtype.int32,
+                                  values=[0, 0, 1, 1, 0, 1, 2])
+        tube_layer = sc.Variable(dims=['tube'],
+                                 dtype=sc.dtype.int32,
+                                 values=np.arange(self._ntube) % tube_layers)
+        layers = tube_layer * straw_layers + straw_layer + pixel_layer
+        return sc.reshape(layers,
+                          dims=[self._dim],
+                          shape=(layers.values.size, )).copy()
