87ac205ecba53dc4a2b0fc5c3f7db114cdc27b8e
Simon Heybrock <simon.heybrock@esss.se>
2020-10-06 07:45:55 +0200

    Avoid need to patch scipp for widget examples

--- sans/convert_helpers.py
+++ sans/convert_helpers.py
@@ -136,6 +136,27 @@ class PlotWidget(w.Box):
         ]
         self.update()
 
+    def _repr_html_(self, input_scope=None):
+        import inspect
+        # Is there a better way to get the scope? The `7` is hard-coded for the
+        # current IPython stack when calling _repr_html_ so this is bound to break.
+        scope = input_scope if input_scope else inspect.stack()[7][0].f_globals
+        from IPython import get_ipython
+        ipython = get_ipython()
+        out = ''
+        for category in ['Variable', 'DataArray', 'Dataset']:
+            names = ipython.magic(f"who_ls {category}")
+            out += f"<details open=\"open\"><summary>{category}s:"\
+                   f"({len(names)})</summary>"
+            for name in names:
+                html = sc.table_html.make_html(eval(name, scope))
+                out += f"<details style=\"padding-left:2em\"><summary>"\
+                       f"{name}</summary>{html}</details>"
+            out += "</details>"
+        from IPython.core.display import display, HTML
+        display(HTML(out))
+
+
     def _on_button_clicked(self, b):
         self.output.clear_output()
         with self.output:
@@ -149,7 +170,7 @@ class PlotWidget(w.Box):
         self._data_selector.options = options
         self.plot_options.clear_output()
         with self.plot_options:
-            sc._repr_html_(self.scope)
+            self._repr_html_(self.scope)
 
 
 class DataCreationWidget(w.Box):
